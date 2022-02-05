8d0c110150a63fd87a6deb8cf1091f48cc82a2b7
seongpil0948 <seongpil0948@gmail.com>
2022-02-05 16:59:01 +0900

    samplePush is done

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"campi/api/core/model"
 	"campi/api/router"
 	"io"
 	"os"
@@ -13,30 +12,7 @@ func main() {
 	// 파일에 로그를 작성합니다.
 	f, _ := os.Create("./public/gin.log")
 	gin.DefaultWriter = io.MultiWriter(f)
-
-	// // https://firebase.google.com/docs/cloud-messaging/send-message?hl=ko
-	// opt := option.WithCredentialsFile("secret/campi-f8278-firebase-adminsdk-hqj7j-ea56733369.json")
-	// ctx := context.Background()
-	// app, _ := firebase.NewApp(ctx, nil, opt)
-	// msgClient, _ := app.Messaging(ctx)
-	// message := &messaging.Message{
-	// 	Data:  map[string]string{"score": "850", "time": "2:45"},
-	// 	Token: config.FcmServerKey,
-	// 	// Topic:        "",
-	// 	// Condition:    "",
-	// }
-	// fmt.Printf("%s", app)
-
-	// // Send a message to the device corresponding to the provided
-	// // registration token.
-	// response, err := msgClient.Send(ctx, message)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// // Response is a message ID string.
-	// fmt.Println("Successfully sent message:", response)
-
 	router := router.InitRoutes()
-	model.SampleOrm()
+	// model.SampleOrm()
 	router.Run() // 서버가 실행 되고 0.0.0.0:8080 에서 요청을 기다립니다.
 }
--- router/index.go
+++ router/index.go
@@ -2,10 +2,14 @@ package router
 
 import (
 	"campi/api/docs"
+	"context"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"     // swagger embed files
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
@@ -18,20 +22,13 @@ func InitRoutes() gin.Engine {
 	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
 		log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
 	}
-
 	SetAuthRoutes(g.Group("auth"))
-	// g.Use(appid.AppIDMiddleWare())
-	// SetHelloRoutes(g)
-	// SetAuthRoutes(g) // SetAuthRoutes invoked
-	// g.Use(token.TokenAuthMiddleWare())  //secure the API From this line to bottom with JSON Auth
-	// g.Use(appid.ValidateAppIDMiddleWare())
-	// SetTaskRoutes(g)
-	// SetUserRoutes(g)
-
 	docs.SwaggerInfo.BasePath = "/api"
 	g.GET("/helloworld", Helloworld)
-	g.GET("/checkIp", checkCLientIp)
+	g.GET("/checkIp", checkClientIp)
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	g.GET("/push/msg", samplePush)
 	return *r
 }
 
@@ -48,8 +45,43 @@ func Helloworld(g *gin.Context) {
 	g.JSON(http.StatusOK, "helloworld")
 }
 
-func checkCLientIp(g *gin.Context) {
+func checkClientIp(g *gin.Context) {
 	six := g.ClientIP()
 	log.Printf("Check Client Ip Ipv6: %v, \n IPv4: %v ", six, net.ParseIP(six).To4())
 	g.JSON(http.StatusOK, "Check Ip API")
+}
+
+func samplePush(g *gin.Context) {
+	println("In Sample Push")
+	ctx := context.Background()
+	app, err := firebase.NewApp(ctx, nil)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	client, err := app.Messaging(ctx)
+	if err != nil {
+		log.Fatalf("error initializing FCM: %v\n", err)
+	}
+
+	message := &messaging.MulticastMessage{
+		Data: map[string]string{
+			"score": "850",
+			"time":  "2:45",
+		},
+		Notification: &messaging.Notification{
+			Title: g.Param("title"),
+			Body:  g.Param("body"),
+		},
+		Tokens: []string{
+			"eMJPDZgwD0dkmdkGmEX3eR:APA91bEwDknN_4eX_Kz3zXJpKn5adPx79t0nSSrxY0FuW_mp-K8ZsFT3eB4AQ0tjujcuYZuYA8hSjY43K1WBg-xMC6uX_7XK90gwYz-rZbyDAdntgxYS4jzwfLhn1Xaa7wWAnEgsE22i",
+			"eQQvrew4S1OLtYH800Thhl:APA91bFsZ8g_31QGFJ8qa0pebgfXPAq7t5OBC9PbTjr5-3XulviqfwUkptY7HK7o2KDHzlixDMrRHZKFYT8KCyfpMX46LZjv8au1iD_C-_mZoQRqBeHCzhxr0cgrSWd4gr9hqVMgK7ly",
+			"c7-VtIhFTKqsz8-X7N5pQP:APA91bGbjcWHn55qSnu2pXTid2qO6Rum3yAiCzgkb7MOhkLBxDwitoyygsrSBcDQL2uKNZmaiBAUiu66FCIow5ZUJ8pkpR0HvtUAV-jM33ABVlInd7-C9a50Avn4KX5h4Ls6_paNfvEO",
+		},
+	}
+	response, err := client.SendMulticast(ctx, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Response is a message ID string.
+	g.JSON(http.StatusOK, fmt.Sprintf("Successfully sent message %v", response))
 }
