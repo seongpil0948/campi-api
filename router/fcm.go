5136910804cc7a8e962eada1afa76dda0b931515
seongpil0948 <seongpil0948@gmail.com>
2022-02-06 22:06:41 +0900

    fcm ok

--- router/fcm.go
+++ router/fcm.go
@@ -9,11 +9,12 @@ import (
 
 	"firebase.google.com/go/messaging"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
 )
 
 func SetFcmRoutes(g *gin.RouterGroup) {
 
-	g.GET("/fcm/SamplePush", SamplePush)
+	g.POST("/fcm/push", SamplePush)
 }
 
 // @Summary Pushing Message
@@ -22,33 +23,59 @@ func SetFcmRoutes(g *gin.RouterGroup) {
 // @Tags Push
 // @Accept json
 // @Produce json
-// @Success 200 {string} SamplePush
-// @Router /fcm/SamplePush [post]
+// @Success 200 {string}
+// @Router /fcm/push [post]
 func SamplePush(g *gin.Context) {
+	var param_tokens, acc_tokens []string
 	println("In Sample Push")
 	app := fbapp.GetFireInstance()
-	client, err := app.Inst.Messaging(app.Ctx)
-	if err != nil {
-		log.Fatalf("error initializing FCM: %v\n", err)
+	msgClient, _ := app.Inst.Messaging(app.Ctx)
+	uIds := g.PostFormArray("userIds")
+	storeClient, _ := app.Inst.Firestore(app.Ctx)
+	param_tokens = g.PostFormArray("tokens")
+	iter := storeClient.Collection("users").Where("userId", "in", uIds).Documents(app.Ctx)
+
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			log.Fatalln(err)
+		}
+		acc_tokens = doc.Data()["messageToken"].([]string)
+		for _, t := range acc_tokens { // Add User Token If not in Param Token
+			exist := false
+			for _, r := range param_tokens {
+				if t == r {
+					exist = true
+				}
+			}
+			if !exist {
+				param_tokens = append(param_tokens, t)
+			}
+		}
+
+		fmt.Println()
 	}
 
 	message := &messaging.MulticastMessage{
 		Data: map[string]string{
-			"data1": g.Param("data1"),
-			"data2": g.Param("data2"),
-			"data3": g.Param("data3"),
+			"data1": g.PostForm("data1"),
+			"data2": g.PostForm("data2"),
+			"data3": g.PostForm("data3"),
 		},
 		Notification: &messaging.Notification{
-			Title: g.Param("title"),
-			Body:  g.Param("body"),
+			Title: g.PostForm("title"),
+			Body:  g.PostForm("body"),
 		},
-		Tokens: []string{
-			"eMJPDZgwD0dkmdkGmEX3eR:APA91bEwDknN_4eX_Kz3zXJpKn5adPx79t0nSSrxY0FuW_mp-K8ZsFT3eB4AQ0tjujcuYZuYA8hSjY43K1WBg-xMC6uX_7XK90gwYz-rZbyDAdntgxYS4jzwfLhn1Xaa7wWAnEgsE22i",
-			"eQQvrew4S1OLtYH800Thhl:APA91bFsZ8g_31QGFJ8qa0pebgfXPAq7t5OBC9PbTjr5-3XulviqfwUkptY7HK7o2KDHzlixDMrRHZKFYT8KCyfpMX46LZjv8au1iD_C-_mZoQRqBeHCzhxr0cgrSWd4gr9hqVMgK7ly",
-			"c7-VtIhFTKqsz8-X7N5pQP:APA91bGbjcWHn55qSnu2pXTid2qO6Rum3yAiCzgkb7MOhkLBxDwitoyygsrSBcDQL2uKNZmaiBAUiu66FCIow5ZUJ8pkpR0HvtUAV-jM33ABVlInd7-C9a50Avn4KX5h4Ls6_paNfvEO",
-		},
+		Tokens: param_tokens,
+		// Tokens: []string{
+		// 	"eMJPDZgwD0dkmdkGmEX3eR:APA91bEwDknN_4eX_Kz3zXJpKn5adPx79t0nSSrxY0FuW_mp-K8ZsFT3eB4AQ0tjujcuYZuYA8hSjY43K1WBg-xMC6uX_7XK90gwYz-rZbyDAdntgxYS4jzwfLhn1Xaa7wWAnEgsE22i",
+		// 	"eQQvrew4S1OLtYH800Thhl:APA91bFsZ8g_31QGFJ8qa0pebgfXPAq7t5OBC9PbTjr5-3XulviqfwUkptY7HK7o2KDHzlixDMrRHZKFYT8KCyfpMX46LZjv8au1iD_C-_mZoQRqBeHCzhxr0cgrSWd4gr9hqVMgK7ly",
+		// 	"c7-VtIhFTKqsz8-X7N5pQP:APA91bGbjcWHn55qSnu2pXTid2qO6Rum3yAiCzgkb7MOhkLBxDwitoyygsrSBcDQL2uKNZmaiBAUiu66FCIow5ZUJ8pkpR0HvtUAV-jM33ABVlInd7-C9a50Avn4KX5h4Ls6_paNfvEO",
+		// },
 	}
-	response, err := client.SendMulticast(app.Ctx, message)
+	response, err := msgClient.SendMulticast(app.Ctx, message)
 	if err != nil {
 		log.Fatalln(err)
 	}
