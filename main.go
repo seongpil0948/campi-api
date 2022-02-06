09dc0898fbd02b56703e4fed01d65bf4cd6c6b12
seongpil0948 <seongpil0948@gmail.com>
2022-02-06 15:50:29 +0900

    rm pg

--- main.go
+++ main.go
@@ -9,10 +9,8 @@ import (
 )
 
 func main() {
-	// 파일에 로그를 작성합니다.
 	f, _ := os.Create("./public/gin.log")
 	gin.DefaultWriter = io.MultiWriter(f)
 	router := router.InitRoutes()
-	// model.SampleOrm()
 	router.Run() // 서버가 실행 되고 0.0.0.0:8080 에서 요청을 기다립니다.
 }
